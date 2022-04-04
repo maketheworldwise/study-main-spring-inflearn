3548617455ccb9c71f3b3914d6fdec1b2601e7df
Kevin <maketheworldwise@gmail.com>
2022-04-05 00:39:23 +0900

    :art: 기본적으로 주어지는 빈 설정 파일
    Spring Boot기준으로, 프로젝트 생성시 자동으로 만들어지는 이 클래스 파일 자체가 빈 설정 파일이라고 볼 수 있음

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -3,10 +3,12 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring Boot 기준으로 기본적으로 설정되어있는 이 파일 자체가 빈 설정 파일
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 
 }
