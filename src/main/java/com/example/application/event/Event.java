9fd221ada56600cff16bfc3c1be2e907255e3a12
Kevin <maketheworldwise@gmail.com>
2022-04-11 02:09:37 +0900

    :art: 어노테이션 기반 Validator

--- src/main/java/com/example/application/event/Event.java
+++ src/main/java/com/example/application/event/Event.java
@@ -1,8 +1,12 @@
 package com.example.application.event;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Event {
 
     int id;
+
+    @NotEmpty
     String title;
 
     public int getId() {
